236f4883849d513d9ff081ea82f1e6a6f3a52d50
Kyankeise <122470088+Kyankeise@users.noreply.github.com>
2023-09-21 16:57:28 +0100

    Add files via upload

--- Netflix-landing-page/js/main.js
+++ Netflix-landing-page/js/main.js
@@ -1,32 +1,32 @@
 // Author: Kyan Keise
-// Date: 19/05/2022
-// Title: Netflix Landing page
+// Date: 19/05/22
+// Title: Netflix landing page
 
-const tabItems = document.querySelectorAll('.tab-item');
-const tabContentItems = document.querySelectorAll('.tab-content-item');
+const tabItems = document.querySelectorAll(".tab-item");
+const tabContentItems = document.querySelectorAll(".tab-content-item");
 
 // Select tab content item
 
 function selectItem(e) {
-    removeBorder();
-    removeShow();
-    // Add border to current tab
-    this.classList.add('tab-border');
-    // Grab content item from DOM
-    const tabContentItem = document.querySelector(`#${this.id}-content`);
+  removeBorder();
+  removeShow();
+  // Add border to current tab
+  this.classList.add("tab-border");
+  // Grab content item from DOM
+  const tabContentItem = document.querySelector(`#${this.id}-content`);
 
-    // add show class
-    tabContentItem.classList.add('show');
+  // add show class
+  tabContentItem.classList.add("show");
 }
 // remove red border
 function removeBorder() {
-    tabItems.forEach(item => item.classList.remove('tab-border'));
+  tabItems.forEach((item) => item.classList.remove("tab-border"));
 }
 
 // remove show to display correct tab
 function removeShow() {
-    tabContentItems.forEach(item => item.classList.remove('show'));
+  tabContentItems.forEach((item) => item.classList.remove("show"));
 }
 
 // Listen for tab click
-tabItems.forEach(item => item.addEventListener('click', selectItem));
+tabItems.forEach((item) => item.addEventListener("click", selectItem));
